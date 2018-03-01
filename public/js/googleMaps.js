883464c0e7c22a4e9469d8489b1f320b496d2a4b
easonychang <yuc247@ucsd.edu>
2018-02-28 16:02:55 -0800

    got google map to work

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,21 +1,21 @@
 function initMap() {
 	// add your code here
-	function initMap() {
+	
 		// Create center marker at UCSD
 		var ucsd_ltlng = {lat:32.880, lng:-117.236};
-	  
+		var cicc_ltlng ={lat:32.884985,lng:-117.241296};
 		// Create a map object and specify the DOM element for display.
 		var map = new google.maps.Map(document.getElementById('map'), {
-		  center: ucsd_ltlng,
+		  center: cicc_ltlng,
 		  zoom: 15
 		});
 	  
-		console.log("in initMap");
+		
 
 		var marker = new google.maps.Marker({
-			position: ucsd_ltlng,
+			position: cicc_ltlng,
 			map: map,
 			title: 'UCSD'
 		});
-	  }
+	
 }
